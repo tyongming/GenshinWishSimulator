97a70719bea25fea25a696b3e8ba78b7e4031044
tyongming <100509459+tyongming@users.noreply.github.com>
2022-06-07 22:01:19 +0800

    Update ShareScreenshot.svelte

--- src/lib/components/utility/ShareScreenshot.svelte
+++ src/lib/components/utility/ShareScreenshot.svelte
@@ -23,7 +23,7 @@
 	let blob;
 	const shareText = "Wow! I'm so lucky when pulling on Wish Simulator, you can try Yours !";
 	let url = '/assets/images/meta-picture.jpg';
-	$: shareLink = `${HOST}/screen/${page}?a=${encodedData}`;
+	$: shareLink = `${HOST}/screen/${page}?a=${encodedData}.png`;
 
 	onMount(() => {
 		isFirstShare = firstShare.check();
